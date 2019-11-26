e39fdc12d51cbedbe466eea5d550b4a3f1c79234
cielolili <cielito.torres@hotmail.com>
2019-11-26 08:58:40 -0500

    añade test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,13 +1,49 @@
 // importamos la función `example`
-import { example } from "../src/data";
+import { filterVaritas, filterPatronus, filterRolesHouses } from '../src/data';
 
-describe('example', () => {
-
+describe('Filtrar patronus', () => {
   it('debería ser una función', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filterPatronus).toBe('function');
+  });
+  it('debería retornar un array con los datos de varitas', () => {
+    const inputPatronus = [{ name: 'Harry Potter', image: 'http://hp-api.herokuapp.com/images/harry.jpg', patronus: 'stag' }, { name: 'Draco Malfoy', image: 'http://hp-api.herokuapp.com/images/draco.jpg', patronus: '' }];
+    const outPutPatronus = [{ name: 'Harry Potter', image: 'http://hp-api.herokuapp.com/images/harry.jpg', patronus: 'stag' }];
+    expect(filterPatronus(inputPatronus, 'patronus', 'stag')).toEqual(outPutPatronus);
+  });
+});
+describe('Filtrar varitas', () => {
+  it('debería ser una función', () => {
+    expect(typeof filterVaritas).toBe('function');
   });
 
-  describe('example', () => {
-    // escribe aquí tu test
+  it('debería retornarme un array wand ', () => {
+    const inputobjeto = [{ name: 'Harry Potter', wand: { wood: 'holly', core: 'phoenix feather' } }, { name: 'Severus Snape' }];
+    const outputobjeto = [{ name: 'Harry Potter', wand: { wood: 'holly', core: 'phoenix feather' } }];
+    expect(filterVaritas(inputobjeto, 'wand')).toEqual(outputobjeto);
+  });
+  it('debería retornar un nuevo array con los datos de varitas', () => {
+    const inputVaritas = [{ wand: { wood: 'holly', core: 'phoenix feather' } }, { wand: { wood: '', core: '' } }];
+    const outPutVaritas = [{ wand: { wood: 'holly', core: 'phoenix feather' } }];
+    expect(filterVaritas(inputVaritas, 'wand', 'wood', 'core')).toEqual(outPutVaritas);
+  });
+});
+describe('Filtrar por casas', () => {
+  it('debería ser una función', () => {
+    expect(typeof filterRolesHouses).toBe('function');
+  });
+  it('debería retornar un array con los datos de casa Gryffindor', () => {
+    const casaGryffindor = [{ name: 'Harry Potter', image: 'http://hp-api.herokuapp.com/images/harry.jpg', house: 'Gryffindor' }, { name: 'Draco Malfoy', image: 'http://hp-api.herokuapp.com/images/draco.jpg', house: 'Slytherin' }];
+    const outPutGryffindor = [{ name: 'Harry Potter', image: 'http://hp-api.herokuapp.com/images/harry.jpg' }];
+    expect(filterRolesHouses(casaGryffindor, 'name', 'Harry Potter')).toEqual(outPutGryffindor);
+  });
+});
+describe('Filtrar por roles', () => {
+  it('debería ser una función', () => {
+    expect(typeof filterRolesHouses).toBe('function');
+  });
+  it('debería retornar un array con los datos de estudiantes', () => {
+    const inputHogwartsStudent = [{ name: 'Harry Potter', image: 'http://hp-api.herokuapp.com/images/harry.jpg' }, { name: 'Minerva McGonagall', image: 'http://hp-api.herokuapp.com/images/mcgonagall.jpg' }];
+    const outPutHogwartsStudent = [{ name: 'Harry Potter', image: 'http://hp-api.herokuapp.com/images/harry.jpg' }];
+    expect(filterRolesHouses(inputHogwartsStudent, 'name', 'Harry Potter')).toEqual(outPutHogwartsStudent);
   });
 });
